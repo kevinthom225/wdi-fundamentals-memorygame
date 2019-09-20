faacc1b6f8ae91bfd4d9926be9f3e525dcc589f2
Kevin Thompson <Kevin@Kevins-Air.attlocal.net>
2019-09-20 13:39:44 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,29 @@
-console.log("Up and running!");
+
 const cards = ["Queen", "Queen", "King", "King"];
 
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2){
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+
+
+function flipCard(cardId){
+
+	console.log("User flipped " + cards[cardId] + ".");
+
+	cardsInPlay.push(cards[cardId]);
+	
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
